a34e4ac7445f99a34b3be5767a3ad0baaa93ccf5
Roberto Delgado <roberto.delgado@gmail.com>
2021-04-29 14:44:08 -1000

    Cleaned up initial files

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = (props) => {
+  return (
+    <div>
+      <p>Sudokrew Technical Interview App</p>
+      <p>Roberto Delgado</p>
+    </div>
+  );
+};
+
+export default Header;

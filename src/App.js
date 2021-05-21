860e7c35295cad01126f34196d74df962f5fb091
RoskiDeluge <roberto.delgado@gmail.com>
2021-05-21 12:06:24 -1000

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Login from "./components/Login";
-import Home from "./components/Home";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
   return (
